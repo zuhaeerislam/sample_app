352ef6d6d95bdc3e397fd3a863441dd6d9a8d041
zuhaeerislam10 <zuhaeerislam@brandeis.edu>
2022-09-12 00:19:34 -0400

    finished the static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#hello"
+  root "static_pages#home"
 end
