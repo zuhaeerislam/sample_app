5c39d9740dc9eae22b731b271b12a34dc2f33596
zuhaeerislam10 <zuhaeerislam@brandeis.edu>
2022-09-23 14:21:13 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'users/new'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   get 'static_pages/faq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "static_pages#home"
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select 'title', 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test 'should get help' do
+    get help_path
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get about" do
-    get static_pages_about_url
+  test 'should get about' do
+    get about_path
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'should get contact' do
+    get contact_path
+    assert_response :success
+    assert_select 'title', 'Contact | Ruby on Rails Tutorial Sample App'
   end
 end
